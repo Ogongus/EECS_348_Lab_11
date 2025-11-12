4ee0678a8c9331b34bd33c27971d78741950f1ce
Ogongus <whjtan4357@gmail.com>
2025-11-11 22:35:11 -0600

    update public

--- Matrix.h
+++ Matrix.h
@@ -27,5 +27,22 @@ class Matrix {
         Matrix operator * (const Matrix& other) const;
 
         // File input
-        
+        bool readFromFile(const std::string& filename, bool second = false);
+
+        // Display
+        void display() const;
+
+        // Diagonal sums
+        int mainDiagonalSum() const;
+        int secondaryDiagonalSum() const;
+
+        // Swap rows / columns by value
+        Matrix swapRows(int r1 = 0, int r2 = 1) const;
+        Matrix swapColumns(int c1 = 0, int c2 = 1) const;
+
+        // Update element
+        Matrix updateElement(int row, int col, int value) const;
+
+        // Getter
+        int getSize() const {return n;}
 }
