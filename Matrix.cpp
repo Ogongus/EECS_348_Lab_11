4709a29428b61ddfc482efb5f30f05f360a1b61c
Ogongus <whjtan4357@gmail.com>
2025-11-11 22:37:27 -0600

    add allocate

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,3 +3,11 @@
 #include <iomanip>
 #include <sstream>
 
+void Matrix::allocate(int size) {
+    n = size;
+    data = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        data[i] = new int[n];
+    }
+}
+
